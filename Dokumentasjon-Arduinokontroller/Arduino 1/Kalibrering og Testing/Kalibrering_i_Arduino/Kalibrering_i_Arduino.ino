b7fd64703659ef55eaa64f5814575deff71500a7
olafdolven <olaf.h.dolven@gmail.com>
2025-02-12 07:08:37 +0100

    ekstra dokumentasjon

--- Dokumentasjon-Arduinokontroller/Arduino 1/Kalibrering og Testing/Kalibrering_i_Arduino/Kalibrering_i_Arduino.ino
+++ Dokumentasjon-Arduinokontroller/Arduino 1/Kalibrering og Testing/Kalibrering_i_Arduino/Kalibrering_i_Arduino.ino
@@ -1,19 +1,22 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN  9
+#define ECHO_PIN     10
+#define MAX_DISTANCE 100 // Maximum distance we want to measure (in centimeters).
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("  A10 = ");
-  Serial.print(analogRead(A0));
-  
-  Serial.print("  A11 = ");
-  Serial.print(analogRead(A1));
-  
-  Serial.print("  A12 = ");
-  Serial.print(analogRead(A2));
-  
-  Serial.print("  A13 = ");
-  Serial.println(analogRead(A3));
+  delay(50);                    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
 
-  delay(100);
+  int distance = sonar.ping_cm(); // Send ping, get distance in cm and print result (0 = outside set distance range)
+
+  //Serial.print("Distance: ");
+  Serial.println(distance);
+  //Serial.println("cm");
 }
